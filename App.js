12321db977dfbfe3c885044cf1f45be716caeb79
Frederik De Bleser <frederik@debleser.be>
2017-10-05 10:13:42 +0200

    Showing a list of our to-do items.

--- App.js
+++ App.js
@@ -1,11 +1,28 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+
+const items = [
+  { key: 1507129580608, text: 'Buy bread', complete: false },
+  { key: 1507129597587, text: 'Walk the dog', complete: false }
+];
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <FlatList
+          style={styles.itemList}
+          data={items}
+          renderItem={this.renderItem}
+        />
+      </View>
+    );
+  }
+
+  renderItem(item) {
+    return (
+      <View style={styles.item}>
+        <Text style={styles.itemText}>{item.item.text}</Text>
       </View>
     );
   }
@@ -16,6 +33,22 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center'
+    justifyContent: 'center',
+    paddingTop: 30
+  },
+  itemList: {
+    width: '100%'
+  },
+  item: {
+    flex: 1,
+    alignItems: 'flex-start',
+    flexDirection: 'row',
+    padding: 10,
+    borderBottomColor: '#eee',
+    borderBottomWidth: 1
+  },
+  itemText: {
+    fontSize: 24,
+    color: '#444'
   }
 });
